afe40a356ad818268c127ebb02ab81e2fbd99e8b
Naama Dayan <namadayan@gmail.com>
2019-12-15 22:42:56 +0200

    Fix imports

--- src/test/java/il/ac/bgu/cs/formalmethodsintro/base/TS/Ex1ExtraPrePostTest.java
+++ src/test/java/il/ac/bgu/cs/formalmethodsintro/base/TS/Ex1ExtraPrePostTest.java
@@ -1,4 +1,4 @@
-package il.ac.bgu.cs.formalmethodsintro.base.michael.TS;
+package il.ac.bgu.cs.formalmethodsintro.base.TS;
 
 import il.ac.bgu.cs.formalmethodsintro.base.FvmFacade;
 import il.ac.bgu.cs.formalmethodsintro.base.TSTestUtils;
